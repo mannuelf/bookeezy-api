79ebb9f4140d26ab115a9b6db61ed839b576ed3f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-10-02 07:29:58 +0200

    mikroOrm: add refelction and scripts

--- src/config/mikro-orm.ts
+++ src/config/mikro-orm.ts
@@ -8,12 +8,14 @@ export default {
   type: 'postgresql',
   clientUrl: 'postgres://root:root@192.168.64.11:30432/bookeezydb',
   entities: [User, Book],
+  entitiesTs: ['./src/entities/**/*.ts'],
   dbName: 'bookeezydb',
   user: 'postgres',
   password: 'root',
+  debug: !__prod__,
   migrations: {
     path: path.join(__dirname, '../migrations'),
     pattern: /^[\w-]+\d+\.[tj]s$/,
+    transactional: true,
   },
-  debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0]; // cast to Parameters to get the types to enable auto completion
